499e4101e3cfa6a63f9787111be6eb6ecc953272
wrwei <ran.wei@york.ac.uk>
2018-12-06 12:09:16 +0000

    example architecture

---
+++ srml.example.architecture/src/srml/example/architecture/SRMLCreationCommand.java
@@ -0,0 +1,14 @@
+package srml.example.architecture;
+
+import org.eclipse.papyrus.infra.architecture.commands.IModelCreationCommand;
+import org.eclipse.papyrus.infra.core.resource.ModelSet;
+
+public class SRMLCreationCommand implements IModelCreationCommand {
+
+	@Override
+	public void createModel(ModelSet modelSet) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
