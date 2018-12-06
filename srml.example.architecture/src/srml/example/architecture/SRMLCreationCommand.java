f25cc5b9a1dfc34689795ff2e2bc72f170cd6f9e
wrwei <ran.wei@york.ac.uk>
2018-12-06 13:31:20 +0000

    some progress

--- srml.example.architecture/src/srml/example/architecture/SRMLCreationCommand.java
+++ srml.example.architecture/src/srml/example/architecture/SRMLCreationCommand.java
@@ -1,14 +1,54 @@
 package srml.example.architecture;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.papyrus.infra.architecture.commands.IModelCreationCommand;
-import org.eclipse.papyrus.infra.core.resource.ModelSet;
+import org.eclipse.papyrus.uml.diagram.common.commands.ModelCreationCommandBase;
+import org.eclipse.papyrus.uml.tools.utils.PackageUtil;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.PackageImport;
+import org.eclipse.uml2.uml.Profile;
+import org.eclipse.uml2.uml.UMLFactory;
+import org.eclipse.uml2.uml.resource.UMLResource;
 
-public class SRMLCreationCommand implements IModelCreationCommand {
+public class SRMLCreationCommand extends ModelCreationCommandBase implements IModelCreationCommand {
+
 
 	@Override
-	public void createModel(ModelSet modelSet) {
+	protected EObject createRootElement() {
 		// TODO Auto-generated method stub
-		
+		return UMLFactory.eINSTANCE.createModel();
 	}
+	
+	@Override
+	protected void initializeModel(EObject owner) {
+		super.initializeModel(owner);
+		Package packageOwner = (Package) owner;
 
+		// Retrieve SysML profile and apply with Sub-profile
+		Profile sysml = (Profile) PackageUtil.loadPackage(URI.createURI(SRMLResource.PROFILE_PATH), owner.eResource().getResourceSet());
+		if (sysml != null) {
+			PackageUtil.applyProfile(packageOwner, sysml, true);
+		}
+
+		Profile standardUMLProfile = (Profile) PackageUtil.loadPackage(URI.createURI(UMLResource.STANDARD_PROFILE_URI), owner.eResource().getResourceSet());
+		if (standardUMLProfile != null) {
+			PackageUtil.applyProfile(packageOwner, standardUMLProfile, true);
+		}
+
+		Package umlPrimitiveTypes = PackageUtil.loadPackage(URI.createURI(UMLResource.UML_PRIMITIVE_TYPES_LIBRARY_URI), owner.eResource().getResourceSet());
+		if (umlPrimitiveTypes != null){
+			PackageImport pi = UMLFactory.eINSTANCE.createPackageImport();
+			pi.setImportedPackage(umlPrimitiveTypes);
+			packageOwner.getPackageImports().add(pi);
+		}
+		
+//		Package sysmlLibrary = PackageUtil.loadPackage(URI.createURI(SysMLResource.LIBRARY_PATH), owner.eResource().getResourceSet());
+//		if (sysmlLibrary != null){
+//			PackageImport pi = UMLFactory.eINSTANCE.createPackageImport();
+//			pi.setImportedPackage(sysmlLibrary);
+//			packageOwner.getPackageImports().add(pi);
+//		}		
+
+	}
 }
